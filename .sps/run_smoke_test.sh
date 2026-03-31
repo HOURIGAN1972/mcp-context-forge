9d4d26fa203bf32c472f45145915e55e704c85ef
Nithin Katta <Nithin.Katta@ibm.com>
2026-03-31 12:25:26 +0100

    test(CYFR-153000): run integration tests in defined order
    Signed-off-by: Nithin Katta <Nithin.Katta@ibm.com>

--- .sps/run_smoke_test.sh
+++ .sps/run_smoke_test.sh
@@ -14,7 +14,7 @@ echo "#############################"
 python3 -m pip install --upgrade pip setuptools
 python3 -m pip install uv --user
 export PATH=/root/.local/bin/:$PATH
-uv run pytest tests/integration -v -s --setup-show --git_token=$(get_env git-token) --mcp_gateway_image_tag="${MCP_GATEWAY_IMAGE_TAG}"
+uv run pytest tests/integration/test_account_user_api.py tests/integration/test_claims_api_v1.py tests/integration/test_gateway_health_api.py tests/integration/test_server_api.py tests/integration/test_tools_api.py tests/integration/test_server_and_tools_apis_destruct -v -s --setup-show --git_token=$(get_env git-token) --mcp_gateway_image_tag="${MCP_GATEWAY_IMAGE_TAG}"
 if [ $? != 0 ]; then
   echo "Integration test failed, exiting";
   exit 1;
