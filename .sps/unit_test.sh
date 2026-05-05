3ac50128eddb928da2987ee636213be86c688fb9
pat <hourigan@ie.ibm.com>
2026-05-05 21:43:41 +0100

    CYFR-163272 Resync from Opensource context forge

--- .sps/unit_test.sh
+++ .sps/unit_test.sh
@@ -90,7 +90,7 @@ make venv install
 echo "############# Installing plugins with verbose output ################"
 . .venv/bin/activate
 echo "Installing plugin packages..."
-python3 -m uv pip install -v \
+pip install -v \
     "cpex-encoded-exfil-detection>=0.2.0" \
     "cpex-pii-filter>=0.2.1" \
     "cpex-rate-limiter>=0.0.4" \
